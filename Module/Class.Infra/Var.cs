ae474298f13fdbd3e6f8c5ec4acb3a82fd042d7c
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-03-21 22:57:27 +0800

    Module

--- Module/Class.Infra/Var.cs
+++
@@ -1,9 +0,0 @@
-namespace Saber.Infra;
-
-public class Var : Any
-{
-    public virtual String Name { get; set; }
-    public virtual Class Class { get; set; }
-    public virtual long Index { get ;set; }
-    public virtual object Any { get; set; }
-}
