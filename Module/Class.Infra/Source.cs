793b9ce26397d5a841b435fdcbb07711b2ed92eb
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-03-22 02:14:29 +0800

    Module

--- Module/Class.Infra/Source.cs
+++
@@ -1,8 +0,0 @@
-namespace Saber.Infra;
-
-public class Source : Any
-{
-    public virtual long Index { get; set; }
-    public virtual String Name { get; set; }
-    public virtual Array Text { get; set; }
-}
