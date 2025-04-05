92721d4713c7f1533f704a52d8eaefcce607e1c3
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-04-05 16:40:46 +0800

    Module

--- Module/Class.Token/SetCreateOperate.cs
+++ Module/Class.Token/SetCreateOperate.cs
@@ -2,7 +2,7 @@ namespace Saber.Token;
 
 public class SetCreateOperate : CreateOperate
 {
-    public override bool ExecuteToken()
+    public override bool ExecuteToken(Range range)
     {
         CreateArg arg;
         arg = this.Create.Arg;
@@ -12,14 +12,8 @@ public class SetCreateOperate : CreateOperate
         Token token;
         token = arg.TokenArray.GetAt(index) as Token;
         token.Row = this.Create.Row;
-
-        Range aa;
-        aa = token.Range; 
-        Range ab;
-        ab = this.Create.LineRange;
-        
-        aa.Index = ab.Index;
-        aa.Count = ab.Count;
+        token.Range.Index = range.Index;
+        token.Range.Count = range.Count;
 
         index = index + 1;
 
