35071e33a799e5d40f81493dc7a03dda8512016b
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-04-07 17:57:58 +0800

    Module

---
+++ Module/Class.Node/NodeState.cl
@@ -0,0 +1,4 @@
+class NodeState : InfraState
+{
+    field prusate Create Create { get { return data; } set { data : value; } }
+}
--- Module/Class.Node/NodeState.cs
+++
@@ -1,6 +0,0 @@
-namespace Saber.Node;
-
-public class NodeState : InfraState
-{
-    public virtual Create Create { get; set; }
-}
