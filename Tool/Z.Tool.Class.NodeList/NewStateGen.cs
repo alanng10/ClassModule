ae55b0fd91492908be57aa158f00a99afe720aa4
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-04-13 17:53:27 +0800

    Tool

--- Tool/Z.Tool.Class.NodeList/NewStateGen.cs
+++ Tool/Z.Tool.Class.NodeList/NewStateGen.cs
@@ -9,4 +9,14 @@ public class NewStateGen : ClassNewStateGen
         this.NewStateSourceFileName = this.S("ToolData/Class/NewStateSource.txt");
         return true;
     }
+
+    protected override String OutputFilePath(String kind)
+    {
+        String fileName;
+        fileName = this.AddClear().AddS("Z_NewState_").Add(kind).AddS(".cl").AddResult();
+
+        String filePath;
+        filePath = this.AddClear().Add(this.OutputFoldPath).Add(this.TextInfra.PathCombine).Add(fileName).AddResult();
+        return filePath;
+    }
 }
