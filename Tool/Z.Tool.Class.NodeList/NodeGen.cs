3796c12ef39f87cbf67ff987dde4a4c00e3b9790
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-04-13 18:17:07 +0800

    Tool

--- Tool/Z.Tool.Class.NodeList/NodeGen.cs
+++ Tool/Z.Tool.Class.NodeList/NodeGen.cs
@@ -2,5 +2,13 @@ namespace Z.Tool.Class.NodeList;
 
 public class NodeGen : ClassNodeGen
 {
-    
+    public override bool Init()
+    {
+        base.Init();
+        this.BoolClassWord = this.S("Bool");
+        this.IntClassWord = this.S("Int");
+        this.OutputFoldPath = this.S("../../Module/Class.Node");
+        this.NodeSourceFileName = this.S("ToolData/Class/NodeSource.txt");
+        return true;
+    }
 }
