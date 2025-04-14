37619137e1e6711bb3721a3fefc72c48ef5de2b2
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-04-14 14:58:26 +0800

    Tool

--- Tool/Z.Tool.Class.NodeList/TravelGen.cs
+++ Tool/Z.Tool.Class.NodeList/TravelGen.cs
@@ -2,5 +2,16 @@ namespace Z.Tool.Class.NodeList;
 
 public class TravelGen : ClassTravelGen
 {
+    public override bool Init()
+    {
+        base.Init();
 
+        this.PathOutput = this.S("../../Module/Class.Node/Travel.cl");
+        return true;
+    }
+
+    protected override String GetPath(String name)
+    {
+        return this.AddClear().AddS("ToolData/Class/Travel").Add(name).AddS(".txt").AddResult();
+    }
 }
