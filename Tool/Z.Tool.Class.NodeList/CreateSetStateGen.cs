10927b68e403f0fc63be18a272422f44aca5f62c
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-04-14 12:23:58 +0800

    Tool

--- Tool/Z.Tool.Class.NodeList/CreateSetStateGen.cs
+++ Tool/Z.Tool.Class.NodeList/CreateSetStateGen.cs
@@ -9,4 +9,28 @@ public class CreateSetStateGen : ClassCreateSetStateGen
         this.SourceFileName = this.S("ToolData/Class/CreateSetStateSource.txt");
         return true;
     }
+
+    protected override bool AddFieldSet(Field field, long index)
+    {
+        String className;
+        className = field.Class;
+
+        this.AddIndent(2);
+
+        this
+            .AddS("node").AddS(".").Add(field.Name)
+            .Add(this.SSpace).AddS(":").Add(this.SSpace);
+
+        this.AddS("cast").Add(this.SSpace).Add(className);
+
+        this.AddS("(")
+        
+        .AddS("k").AddS(".");
+
+        this.AddArgFieldName(index, className);
+
+        this.AddS(")").AddS(";").AddLine();
+
+        return true;
+    }
 }
