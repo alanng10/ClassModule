2c9031cca7a240b86c1ab874e78e205474e1aedd
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-04-14 13:00:42 +0800

    Tool

--- Tool/Z.Tool.Class.NodeList/NodeKindListGen.cs
+++ Tool/Z.Tool.Class.NodeList/NodeKindListGen.cs
@@ -12,8 +12,6 @@ public class NodeKindListGen : SourceGen
         this.ItemClassName = this.S("NodeKind");
         this.ArrayClassName = this.S("Array");
         this.Export = true;
-        this.ClassFileName = this.S("ToolData/Class/ClassNodeKind.txt");
-        this.InitMethodFileName = this.S("ToolData/Class/InitMaideNodeKind.txt");
         this.AddMethodFileName = this.S("ToolData/Class/AddMaideNodeKind.txt");
         this.OutputFilePath = this.S("../../Module/Class.Node/NodeKindList.cl");
         return true;
