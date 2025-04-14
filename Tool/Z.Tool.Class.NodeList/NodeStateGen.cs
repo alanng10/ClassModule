047479b2e8cefd8fe0a5438521510b8f5bc8784e
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-04-14 11:52:19 +0800

    Tool

--- Tool/Z.Tool.Class.NodeList/NodeStateGen.cs
+++ Tool/Z.Tool.Class.NodeList/NodeStateGen.cs
@@ -2,4 +2,11 @@ namespace Z.Tool.Class.NodeList;
 
 public class NodeStateGen : ClassNodeStateGen
 {
+    public override bool Init()
+    {
+        base.Init();
+        this.OutputFoldPath = this.S("../../Module/Class.Node");
+        this.NodeStateSourceFileName = this.S("ToolData/Class/NodeStateSource.txt");
+        return true;
+    }
 }
