9dd3a26a8c390f4610d83684b634549f550d0e50
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-04-25 13:58:31 +0800

    Module

--- Module/Class.Node/Create.cs
+++ Module/Class.Node/Create.cs
@@ -1311,7 +1311,7 @@ public class Create : ClassCreate
         start = range.Start;
         end = range.End;
 
-        if (!(start + 1 == end))
+        if (!(this.Count(start, end) == 1))
         {
             return null;
         }
