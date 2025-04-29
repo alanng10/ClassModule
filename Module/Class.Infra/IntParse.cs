6c08221d86464f95cfc5ccfed0d27d3dcf3e9c31
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-04-29 10:20:29 +0800

    Module

--- Module/Class.Infra/IntParse.cs
+++ Module/Class.Infra/IntParse.cs
@@ -73,7 +73,7 @@ public class IntParse : TextAdd
         }
 
         long a;
-
+        a = 0;
         if (negate == 0)
         {
             a = k;
@@ -126,6 +126,86 @@ public class IntParse : TextAdd
 
         long a;
         a = value;
+        return a;
+    }
+
+    public virtual long SignValue(Text text)
+    {
+        long count;
+        count = text.Range.Count;
+
+        if (count < 4)
+        {
+            return -1;
+        }
+
+        Data data;
+        data = text.Data;
+        long index;
+        index = text.Range.Index;
+
+        if (!(this.TextInfra.DataCharGet(data, index) == '0'))
+        {
+            return -1;
+        }
+        if (!(this.TextInfra.DataCharGet(data, index + 1) == 's'))
+        {
+            return -1;
+        }
+
+        long kaa;
+        kaa = this.TextInfra.DataCharGet(data, index + 2);
+
+        long negate;
+        negate = this.IntSign(kaa);
+
+        if (negate == -1)
+        {
+            return -1;
+        }
+
+        long indexA;
+        long countA;
+        indexA = index + 3;
+        countA = count - 3;
+        this.TextA.Data = data;
+        this.TextA.Range.Index = indexA;
+        this.TextA.Range.Count = countA;
+
+        long k;
+        k = this.IntText(text, 10);
+
+        if (k == -1)
+        {
+            return -1;
+        }
+
+        long max;
+        max = 0;
+        if (negate == 0)
+        {
+            max = this.ClassInfra.IntSignPositeMax;
+        }
+        if (negate == 1)
+        {
+            max = this.ClassInfra.IntSignNegateMax;
+        }
+
+        if (max < k)
+        {
+            return -1;
+        }
+
+        long a;
+        a = 0;
+        if (negate == 0)
+        {
+            a = k;
+        }
+        if (negate == 1)
+        {
+            a = -k;
+        }
         return a;
     }
 
