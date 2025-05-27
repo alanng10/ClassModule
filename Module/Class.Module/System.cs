b16217c578b7493915905eb8137ef56664871cf5
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-05-27 15:13:09 +0800

    Module

--- Module/Class.Module/System.cs
+++
@@ -1,9 +0,0 @@
-namespace Saber.Module;
-
-public class System : Any
-{
-    public virtual Class Any { get; set; }
-    public virtual Class Bool { get; set; }
-    public virtual Class Int { get; set; }
-    public virtual Class String { get; set; }
-}
