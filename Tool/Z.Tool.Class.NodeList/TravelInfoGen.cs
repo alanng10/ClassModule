c1ada72c2019900a9ffc0466e5140376c56a1041
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-06-06 17:00:40 +0800

    Tool

--- Tool/Z.Tool.Class.NodeList/TravelInfoGen.cs
+++ Tool/Z.Tool.Class.NodeList/TravelInfoGen.cs
@@ -51,6 +51,55 @@ public class TravelInfoGen : ClassTravelGen
         return a;
     }
 
+    protected override String ArrayState(GenClass varClass, String varName)
+    {
+        StringAdd h;
+        h = new StringAdd();
+        h.Init();
+
+        StringAdd hh;
+        hh = this.StringAdd;
+
+        this.StringAdd = h;
+
+        this.AddClear();
+
+        this.AddLine();
+
+        Table table;
+        table = varClass.Derive;
+
+        Iter iter;
+        iter = table.IterCreate();
+        table.IterSet(iter);
+
+        while (iter.Next())
+        {
+            GenClass aa;
+            aa = iter.Value as GenClass;
+
+            String className;
+            className = aa.Name;
+
+            Text k;
+            k = this.TextCreate(this.TextArray);
+            k = this.Place(k, "#VarName#", varName);
+            k = this.Place(k, "#ClassName#", className);
+
+            String ka;
+            ka = this.StringCreate(k);
+
+            this.Add(ka);
+        }
+
+        String a;
+        a = this.AddResult();
+
+        this.StringAdd = hh;
+
+        return a;
+    }
+
     protected override String DeclareClassName(String className)
     {
         Text k;
