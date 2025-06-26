7fe2793baada7b4f85aad48220ffb17946eaa876
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-06-26 22:24:47 +0800

    Module

--- Module/Class.Binary/Read.cs
+++ Module/Class.Binary/Read.cs
@@ -744,14 +744,12 @@ public class Read : Any
 
     protected virtual Array ExecuteMaideArray()
     {
-        long o;
-        o = this.ExecuteCount();
-        if (o == -1)
+        long count;
+        count = this.ExecuteCount();
+        if (count == -1)
         {
             return null;
         }
-        long count;
-        count = o;
 
         Array array;
         array = this.ExecuteArray(count);
@@ -770,7 +768,9 @@ public class Read : Any
             {
                 return null;
             }
+
             this.Operate.ExecuteArrayItemSet(array, i, a);
+
             i = i + 1;
         }
         return array;
