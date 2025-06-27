7832e4a8f87ca0bdf6bc60592ed670ed2645456e
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-06-27 11:06:57 +0800

    Script

--- Script/Module/Make.cmd
+++ Script/Module/Make.cmd
@@ -5,5 +5,6 @@ call Script\Module\MakeModule Port
 call Script\Module\MakeModule Token
 call Script\Module\MakeModule Node
 call Script\Module\MakeModule Module
+call Script\Module\MakeModule Binary
 call Script\Module\MakeModule Console
 call Script\Module\MakeModule Test
