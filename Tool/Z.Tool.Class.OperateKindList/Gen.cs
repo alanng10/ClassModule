7ddfb77d6c3a07f79f7464cda1d2164879bd92e2
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-06-30 10:23:37 +0800

    Tool

--- Tool/Z.Tool.Class.OperateKindList/Gen.cs
+++ Tool/Z.Tool.Class.OperateKindList/Gen.cs
@@ -17,4 +17,50 @@ public class Gen : SourceGen
         this.OutputFilePath = this.S("../../Module/Class.Binary/OperateKindList.cl");
         return true;
     }
+
+    protected override TableEntry GetItemEntry(String line)
+    {
+        Text kka;
+        kka = this.TextCreate(this.S(" "));
+
+        Text k;
+        k = this.TextCreate(line);
+
+        Array array;
+        array = this.TextLimit(k, kka);
+
+        Text ka;
+        Text kb;
+        ka = array.GetAt(0) as Text;
+        kb = array.GetAt(1) as Text;
+
+        String index;
+        index = this.StringCreate(ka);
+
+        String text;
+        text = this.StringCreate(kb);
+
+        Value value;
+        value = new Value();
+        value.Init();
+        value.Text = text;
+
+        TableEntry entry;
+        entry = new TableEntry();
+        entry.Init();
+        entry.Index = index;
+        entry.Value = value;
+        return entry;
+    }
+
+    protected override bool AddInitFieldAddItem(String index, object value)
+    {
+        Value a;
+        a = (Value)value;
+        this.AddS("AddItem")
+            .AddS("(")
+            .AddS("\"").Add(a.Text).AddS("\"")
+            .AddS(")");
+        return true;
+    }
 }
