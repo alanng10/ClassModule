4e9e3ea7687f8a8472ea178bc6138a7e36d6b807
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-11-04 21:20:04 +0800

    Script

--- Script/Module/Make.cmd
+++ Script/Module/Make.cmd
@@ -1,10 +1,10 @@
 @echo off
 
-call ..\Script\Module\Make ClassModule Class.Infra
-call ..\Script\Module\Make ClassModule Class.Binary
-call ..\Script\Module\Make ClassModule Class.Port
-call ..\Script\Module\Make ClassModule Class.Token
-call ..\Script\Module\Make ClassModule Class.Node
-call ..\Script\Module\Make ClassModule Class.Module
-call ..\Script\Module\Make ClassModule Class.Console
-call ..\Script\Module\Make ClassModule Class.Test
+call ..\Script\Module\MakeSaber ClassModule Class.Infra
+call ..\Script\Module\MakeSaber ClassModule Class.Binary
+call ..\Script\Module\MakeSaber ClassModule Class.Port
+call ..\Script\Module\MakeSaber ClassModule Class.Token
+call ..\Script\Module\MakeSaber ClassModule Class.Node
+call ..\Script\Module\MakeSaber ClassModule Class.Module
+call ..\Script\Module\MakeSaber ClassModule Class.Console
+call ..\Script\Module\MakeSaber ClassModule Class.Test
